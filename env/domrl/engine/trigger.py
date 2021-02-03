3be74fdb07901d436daf745cb926d101a1950c39
aronsar <aronsar@ad3.ucdavis.edu@interaction.cs.ucdavis.edu>
2021-02-03 14:11:13 -0800

    removed duplicate env

--- env/domrl/engine/trigger.py
+++
@@ -1,9 +0,0 @@
-import domrl.engine.logger as log
-
-
-class Trigger:
-    def condition(self, event: log.Event):
-        raise NotImplementedError('`Trigger` must be subclassed to have condition')
-
-    def apply(self, event: log.Event, state):
-        raise NotImplementedError('`Trigger` must be subclassed to be applied')
